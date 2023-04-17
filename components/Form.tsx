35da1e154b1c54d2d0f37f893e13172c5158209c
Azamat <augustintherome@gmail.com>
2023-04-17 06:54:47 +0600

    Wip

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,19 +1,56 @@
 import * as Primitives from "@radix-ui/react-form";
-import { ComponentPropsWithoutRef, forwardRef } from "react";
+import clsx from "clsx";
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 
 export const Form = Primitives.Root;
 
-export const Field = Primitives.Field;
+export const Field = forwardRef<
+  ElementRef<typeof Primitives.Field>,
+  ComponentPropsWithoutRef<typeof Primitives.Field>
+>((props, ref) => {
+  return <Primitives.Field {...props} ref={ref} className="mt-6" />;
+});
 
 export const Label = forwardRef<
-  HTMLLabelElement,
-  ComponentPropsWithoutRef<"label">
->(({ children, ...props }, ref) => {
+  ElementRef<typeof Primitives.Label>,
+  ComponentPropsWithoutRef<typeof Primitives.Label>
+>(({ className, ...props }, ref) => {
   return (
-    <Primitives.Label {...props} ref={ref} asChild>
-      <label className="block text-sm font-medium leading-6 text-gray-900">
-        {children}
-      </label>
-    </Primitives.Label>
+    <Primitives.Label
+      {...props}
+      ref={ref}
+      className={clsx(
+        "block text-sm font-medium leading-6 text-zinc-900",
+        className
+      )}
+    />
+  );
+});
+
+export const Input = forwardRef<
+  ElementRef<typeof Primitives.Control>,
+  ComponentPropsWithoutRef<typeof Primitives.Control>
+>(({ className, ...props }, ref) => {
+  return (
+    <Primitives.Control
+      {...props}
+      ref={ref}
+      className={clsx(
+        "block w-full rounded-md border-0 py-1.5 text-zinc-900 shadow-sm ring-1 ring-zinc-300 placeholder:text-zinc-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 disabled:cursor-not-allowed disabled:bg-zinc-50 disabled:text-zinc-500 disabled:ring-zinc-200 sm:text-sm sm:leading-6"
+      )}
+    />
+  );
+});
+
+export const Message = forwardRef<
+  HTMLElement,
+  ComponentPropsWithoutRef<typeof Primitives.Message>
+>(({ className, ...props }, ref) => {
+  return (
+    <Primitives.Message
+      {...props}
+      ref={ref}
+      className={clsx("text-sm text-zinc-500", className)}
+    />
   );
 });
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import api from "../api";
@@ -26,61 +25,51 @@ export default function Login() {
   };
 
   return (
-    <>
-      <Head>
-        <title>Sign In - TaxPal</title>
-      </Head>
-      <AuthLayout>
-        <div className="flex flex-col">
-          <Link href="/" aria-label="Home">
-            <Logo className="h-10 w-auto" />
-          </Link>
-          <div className="mt-20">
-            <h2 className="text-lg font-semibold text-gray-900">
-              Войдите в свой аккаунт
-            </h2>
-            <p className="mt-2 text-sm text-gray-700">
-              Еще нет аккаунта?{" "}
-              <Link
-                href="/register"
-                className="font-medium text-blue-600 hover:underline"
-              >
-                Зарегистрируйтесь
-              </Link>{" "}
-              сейчас.
-            </p>
-          </div>
-        </div>
-        <form onSubmit={onSubmit} className="mt-10 grid grid-cols-1 gap-y-8">
-          <TextField
-            label="Электронная почта"
-            id="email"
-            name="email"
-            type="email"
-            defaultValue={router.query.email}
-            autoComplete="email"
-            required
-          />
-          <TextField
-            label="Пароль"
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            required
-          />
-          <div>
-            <Button
-              type="submit"
-              variant="solid"
-              color="blue"
-              className="w-full"
+    <AuthLayout>
+      <div className="flex flex-col">
+        <Link href="/" aria-label="Home">
+          <Logo className="h-10 w-auto" />
+        </Link>
+        <div className="mt-20">
+          <h2 className="text-lg font-semibold text-gray-900">
+            Войдите в свой аккаунт
+          </h2>
+          <p className="mt-2 text-sm text-gray-700">
+            Еще нет аккаунта?{" "}
+            <Link
+              href="/register"
+              className="font-medium text-blue-600 hover:underline"
             >
-              Войти
-            </Button>
-          </div>
-        </form>
-      </AuthLayout>
-    </>
+              Зарегистрируйтесь
+            </Link>{" "}
+            сейчас.
+          </p>
+        </div>
+      </div>
+      <form onSubmit={onSubmit} className="mt-10 grid grid-cols-1 gap-y-8">
+        <TextField
+          label="Электронная почта"
+          id="email"
+          name="email"
+          type="email"
+          defaultValue={router.query.email}
+          autoComplete="email"
+          required
+        />
+        <TextField
+          label="Пароль"
+          id="password"
+          name="password"
+          type="password"
+          autoComplete="current-password"
+          required
+        />
+        <div>
+          <Button type="submit" variant="solid" color="blue" className="w-full">
+            Войти
+          </Button>
+        </div>
+      </form>
+    </AuthLayout>
   );
 }
