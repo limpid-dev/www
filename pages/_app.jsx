806c4d621e1610af05aaacbdf4fbdbcbea1e38e9
Azamat <augustintherome@gmail.com>
2023-04-30 20:48:32 +0600

    Fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,10 +6,15 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {typeof window !== "undefined" &&
+        {/* {typeof window !== "undefined" &&
           window.location.hostname === "limpid.kz" && (
             <script async defer data-domain="limpid.kz" src="/js/script.js" />
-          )}
+          )} */}
+        <script
+          defer
+          data-domain="limpid.kz"
+          src="https://limpid.kz/js/script.js"
+        />
       </Head>
       <Component {...pageProps} />
     </>
