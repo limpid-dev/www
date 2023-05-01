661dbe2bc081936a505477d6ae44ef81ba829179
Azamat <augustintherome@gmail.com>
2023-05-02 03:23:36 +0600

    Added font inter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import "focus-visible";
 import "../tailwind.css";
 import { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 import Script from "next/script";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["cyrillic", "latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={inter.variable}>
       <Component {...pageProps} />
       <Script defer data-domain="limpid.kz" src="/js/script.js" />
-    </>
+    </div>
   );
 }
