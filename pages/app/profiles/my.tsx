7b37cce454d6882f0f5c2157493d1b927256d28e
AlmazDD <iheardthatwas@gmail.com>
2023-05-11 13:19:06 +0600

    select

--- pages/app/profiles/my.tsx
+++ pages/app/profiles/my.tsx
@@ -187,7 +187,7 @@ export default function MyProfiles({ data }: Props) {
                         </div>
                       </div>
                     </DialogTrigger>
-                    <DialogContent>
+                    <DialogContent className="overflow-auto h-full">
                       <DialogHeader>
                         <DialogTitle> Создание профиля</DialogTitle>
                         <DialogDescription>
