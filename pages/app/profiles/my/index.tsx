2aa0596ec4211bd1b21355c02c4046c15e38d3ca
AlmazDD <iheardthatwas@gmail.com>
2023-05-25 14:57:17 +0600

    ses

--- pages/app/profiles/my/index.tsx
+++ pages/app/profiles/my/index.tsx
@@ -1,8 +1,4 @@
-import {
-  GetServerSideProps,
-  GetServerSidePropsContext,
-  GetStaticProps,
-} from "next";
+import { GetStaticProps } from "next";
 import api from "../../../../api";
 import PaginatedMyProfiles from "./[page]";
 
@@ -11,33 +7,18 @@ const tabs = [
   { name: "Мои профили", href: "/app/profiles/my", current: true },
 ];
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext<{
-    id: string;
-  }>
-) => {
-  const session = await api.session.show({
-    headers: {
-      Cookie: context.req.headers.cookie!,
-    },
-    credentials: "include",
+export const getStaticProps: GetStaticProps = async () => {
+  const profiles = await api.profiles.index({
+    page: 1,
+    perPage: 9,
   });
-  if (session) {
-    const profiles = await api.profiles.index({
-      page: 1,
-      perPage: 9,
-      filters: {
-        userId: session.data!.id,
-      },
-    });
-    return {
-      props: {
-        profiles: profiles.data,
-        totalProfiles: profiles.meta?.total,
-        currentPage: 1,
-      },
-    };
-  }
+  return {
+    props: {
+      profiles: profiles.data,
+      totalProfiles: profiles.meta?.total,
+      currentPage: 1,
+    },
+  };
 };
 
 export default function MyProfiles({
