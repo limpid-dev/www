a233e5ddf1588202848855f453fe4597592fc8ad
Almaz Nurgali <almaznurgali@Almazs-MacBook-Pro.local>
2023-06-09 11:04:13 +0600

    test

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import { components, paths } from "./api-paths";
 
-// const API_BASE_URL = "https://limpid.kz/api";
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://limpid.kz/api";
+// const API_BASE_URL = "http://localhost:3000/api";
 
 class APIClient {
   private axiosInstance = axios.create({
