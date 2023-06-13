a6300bf3d7364bf65dd090187e71e96a553b30eb
Almaz <iheardthatwas@gmail.com>
2023-06-13 18:10:40 +0600

    tst

--- pages/app/projects/create/files.tsx
+++ pages/app/projects/create/files.tsx
@@ -13,96 +13,79 @@ interface FormValues {
   presentation?: FileList;
   business_plan?: FileList;
 }
+
 export default function Create() {
-  const fileMaxSize = 1 * 1024 * 1024;
   const router = useRouter();
   const { projectId } = router.query;
   const parsedProjectId = Number.parseInt(projectId as string, 10) as number;
 
   const {
     register,
-    setError,
     handleSubmit,
-    clearErrors,
     formState: { errors },
+    setError,
   } = useForm<FormValues>({});
 
   const onSubmit = async (data1: FormValues) => {
-    const { data } = await api.updateProject(
-      { project_id: parsedProjectId },
-      {
-        logo: data1.logo[0],
-        video_introduction: data1.video_introduction[0],
-        presentation: data1.presentation[0],
-        business_plan: data1.business_plan[0],
+    try {
+      console.log(data1);
+      const { data } = await api.updateProject(
+        { project_id: parsedProjectId },
+        {
+          logo: data1.logo[0],
+          video_introduction: data1.video_introduction[0],
+          presentation: data1.presentation[0],
+          business_plan: data1.business_plan[0],
+        }
+      );
+      if (data.data) {
+        await router.push(`/app/projects/${parsedProjectId}`);
       }
-    );
-    if (data.data?.id) {
-      await router.push(`/app/projects/${parsedProjectId}`);
-    }
-  };
-
-  const supportedFileTypes = {
-    logo: ["image/jpeg", "image/png"],
-    video_introduction: ["video/mp4"],
-    business_plan: [
-      "application/pdf",
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    ],
-    presentation: ["application/pdf", "application/vnd.ms-powerpoint"],
-  };
-
-  const handleFileChange = (inputName) => (event) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      const fileMaxSize = getFileMaxSize(inputName);
-      const supportedTypes = supportedFileTypes[inputName];
-      if (file.size > fileMaxSize) {
-        setError(inputName, {
-          type: "validate",
-          message: `File size should not exceed ${formatFileSize(fileMaxSize)}`,
-        });
-      } else if (!supportedTypes.includes(file.type)) {
-        // Set error message for file type validation
-        setError(inputName, {
-          type: "validate",
-          message: `Unsupported file type. Please choose ${supportedTypes.join(
-            ", "
-          )}`,
-        });
+    } catch (error) {
+      if (error.response && error.response.status === 422) {
+        const { data } = error.response;
+        if (data.errors && Array.isArray(data.errors)) {
+          data.errors.forEach((error: any) => {
+            const { field, message } = error;
+            switch (field) {
+              case "logo":
+                setError(field, {
+                  type: "manual",
+                  message: "Изображение не правильно",
+                });
+                break;
+              case "video_introduction":
+                setError(field, {
+                  type: "manual",
+                  message: "Не правильное видео",
+                });
+                break;
+              case "presentation":
+                setError(field, {
+                  type: "manual",
+                  message: "Презентация не правильна",
+                });
+                break;
+              case "business_plan":
+                setError(field, {
+                  type: "manual",
+                  message: "Не правильный бизнес план",
+                });
+                break;
+              default:
+                break;
+            }
+          });
+        }
       } else {
-        clearErrors(inputName);
+        console.log("An unexpected error occurred:", error);
       }
     }
-  };
-
-  const getFileMaxSize = (inputName) => {
-    switch (inputName) {
-      case "logo":
-        return 1 * 1024 * 1024;
-      case "video_introduction":
-        return 128 * 1024 * 1024;
-      case "business_plan":
-        return 5 * 1024 * 1024;
-      case "presentation":
-        return 8 * 1024 * 1024;
-      default:
-        return 0;
-    }
-  };
-
-  const formatFileSize = (sizeInBytes) => {
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
-    const i = Math.floor(Math.log(sizeInBytes) / Math.log(1024));
-    if (i === 0) {
-      return `${sizeInBytes} ${sizes[i]}`;
-    }
-    return `${(sizeInBytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
   };
 
   return (
     <>
-      <div className="min-h-screen  bg-slate-50">
+      <div className="min-h-screen bg-slate-50">
         <Navigation />
         <div className="container mx-auto mb-4 max-w-screen-xl px-5 pt-8">
           <div className="rounded-lg bg-white shadow-sm">
@@ -111,26 +94,19 @@ export default function Create() {
                 Общие данные
               </div>
               <div className="border-b flex items-center justify-center border-slate-100 py-8 flex-1 whitespace-nowrap font-semibold text-lime-600 text-lg sm:text-xl">
-                Документа∏ия
+                Документация
               </div>
             </div>
             <div className="grid min-h-[500px] items-center justify-center border-none p-10 max-w-screen-md mx-auto">
               <form onSubmit={handleSubmit(onSubmit)} className="pb-4">
-                {/* Rest of your code */}
                 <div className="grid items-center justify-center gap-6">
                   <div className="grid sm:grid-cols-2 gap-10">
                     <div className="grid gap-y-3">
                       <p className="text-2xl font-semibold">Логотип</p>
                       <p className="text-sm">Загрузите логотип проекта</p>
-                      <Input
-                        type="file"
-                        {...register("logo")}
-                        onChange={handleFileChange("logo")}
-                      />
+                      <Input type="file" {...register("logo")} />
                       {errors.logo && (
-                        <span className="text-sm text-red-600">
-                          {errors.logo.message}
-                        </span>
+                        <p className="text-red-500">{errors.logo.message}</p>
                       )}
                     </div>
                     <div className="grid gap-y-3">
@@ -138,46 +114,38 @@ export default function Create() {
                       <p className="text-sm">
                         Загрузите короткое видео вашего проекта
                       </p>
-                      <Input
-                        type="file"
-                        {...register("video_introduction")}
-                        onChange={handleFileChange("video_introduction")}
-                      />
+                      <Input type="file" {...register("video_introduction")} />
                       {errors.video_introduction && (
-                        <span className="text-sm text-red-600">
+                        <p className="text-red-500">
                           {errors.video_introduction.message}
-                        </span>
+                        </p>
                       )}
                     </div>
                     <div className="grid gap-y-3">
                       <p className="text-2xl font-semibold">Бизнес план</p>
                       <p className="text-sm">Загрузите ваш бизнесплан</p>
-                      <Input
-                        type="file"
-                        {...register("business_plan")}
-                        onChange={handleFileChange("business_plan")}
-                      />
+                      <Input type="file" {...register("business_plan")} />
                       {errors.business_plan && (
-                        <span className="text-sm text-red-600">
+                        <p className="text-red-500">
                           {errors.business_plan.message}
-                        </span>
+                        </p>
                       )}
                     </div>
                     <div className="grid gap-y-3">
                       <p className="text-2xl font-semibold">Презентация</p>
                       <p className="text-sm">Загрузите презентацию проекта</p>
-                      <Input
-                        type="file"
-                        {...register("presentation")}
-                        onChange={handleFileChange("presentation")}
-                      />
+                      <Input type="file" {...register("presentation")} />
                       {errors.presentation && (
-                        <span className="text-sm text-red-600">
+                        <p className="text-red-500">
                           {errors.presentation.message}
-                        </span>
+                        </p>
                       )}
                     </div>
                   </div>
+                </div>
+                <div className="flex justify-end gap-3 pt-14">
+                  <Button variant="outline">Отмена</Button>
+                  <Button type="submit">Сохранить</Button>
                 </div>
               </form>
             </div>
