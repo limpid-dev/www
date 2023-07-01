3c71639375fb476fea80bcaea70327d162595182
Azamat Zulpykhar <spender.poised-0q@icloud.com>
2023-07-01 06:51:41 +0600

    WIP

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -113,7 +113,7 @@ const navigation = [
   { name: "Организации", href: "/app/organizations" },
   { name: "Продажи", href: "/app/auctions" },
   { name: "Закупки", href: "/app/tenders" },
-  // // { name: "Чаты", href: "/app/chats" },
+  { name: "Чаты", href: "/app/chats" },
 ];
 
 const userNavigation = [{ name: "Настройки", href: "/app/settings" }];
